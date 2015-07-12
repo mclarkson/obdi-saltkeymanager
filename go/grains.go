2e256ddcc6194b1a2dfd23e36df4d89bd9f0133b
Mark Clarkson <mark.clarkson@smorg.co.uk>
2015-07-12 05:46:38 +0100

    Commented out client.Timeout to support Go < 1.3

--- go/grains.go
+++ go/grains.go
@@ -118,7 +118,8 @@ func GET(url, endpoint string) (r *http.Response, e error) {
     TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
   }
   client := &http.Client{Transport: tr}
-  client.Timeout = 8 * 1e9
+  // Not available in Go<1.3
+  //client.Timeout = 8 * 1e9
 
   //fmt.Printf("\n%s/api/%s\n",url,endpoint)
   for strings.HasSuffix(url, "/") {
@@ -169,7 +170,8 @@ func POST(jsondata []byte, url, endpoint string) (r *http.Response, e error) {
     TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
   }
   client := &http.Client{Transport: tr}
-  client.Timeout = 8 * 1e9
+  // Not available in Go<1.3
+  //client.Timeout = 8 * 1e9
 
   //fmt.Printf("\n%s/api/%s\n",url,endpoint)
   for strings.HasSuffix(url, "/") {
